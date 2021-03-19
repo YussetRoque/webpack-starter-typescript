1707ec9d874ddecd23712aa10d471b6f920f311d
Yusset <Yussetroquecemas@gmail.com>
2021-03-18 22:22:36 -0400

    Insetar data en firestore

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,22 @@
 
 
 
-import './firebase/config';
+import db from './firebase/config';
+
+const usuario = {
+    nombre: 'Juana',
+    activo: true,
+    FechaNaci: 0
+
+}
+
+// insert into usuarios ......
+
+// db.collection('usuarios')
+//    .add( usuario )
+//    .then( docRef => {
+//        console.log( docRef.delete() )
+
+ //   })
+
+ //   .catch( e => console.log ('error', e ))
